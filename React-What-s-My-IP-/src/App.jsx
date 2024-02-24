7903f15d2f468f95ee83472c6b1215d4f2d747d0
GG-Gx <78389994+GG-Gx@users.noreply.github.com>
2024-02-24 16:30:09 +0100

    1630

--- React-What-s-My-IP-/src/App.jsx
+++ React-What-s-My-IP-/src/App.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import { ChakraProvider } from '@chakra-ui/react'
 import './App.css'
-
 import Ip from './components/Ip'
 
 function App() {
@@ -10,7 +9,6 @@ function App() {
     <ChakraProvider>
       <Ip />
     </ChakraProvider>
-    
   )
 }
 
--- React-What-s-My-IP-/src/components/Ip.jsx
+++ React-What-s-My-IP-/src/components/Ip.jsx
@@ -1,18 +1,23 @@
 import React, {useState, useEffect} from 'react';
-import { Box, Card, CardBody, ChakraProvider, Flex, Text } from '@chakra-ui/react';
-import { MapContainer, TileLayer, useMap, Marker,Popup } from 'react-leaflet'
+import { Box, Card, CardBody, ChakraProvider, Flex, Text, Image, Center, List} from '@chakra-ui/react';
+import { MapContainer, TileLayer, useMap, Marker,Popup,  } from 'react-leaflet'
 import 'leaflet/dist/leaflet.css';
+import { DateTime } from "luxon";
+
+
 
 
 
 
 const Ip = () => {
   const [data, setData] = useState('');
+
   useEffect(() => {
     fetch(`https://geo.ipify.org/api/v1?apiKey=${import.meta.env.VITE_API_KEY}`)
       .then((response) => response.json())
       .then((data) => setData(data))
       .catch((error) => console.error('Error:', error));
+
   }, []);
 
 
@@ -23,21 +28,42 @@ const Ip = () => {
   const lng = location ? location.lng : '';
   const country = location ? location.country : '';
   const city = location ? location.city : '';
+  const flag = `https://flagcdn.com/w20/${country.toLowerCase()}.png`;
 
+
+  const localNow = DateTime.local();
+ 
+  const buenosAiresNow = DateTime.now().setZone('America/Argentina/Buenos_Aires');
+ 
+  const timeDiff = buenosAiresNow.diff(localNow, ['days', 'hours']);
+
+  const localNowString = localNow.toLocaleString(DateTime.DATETIME_FULL);
+  const timeDiffString = timeDiff.toFormat("d 'days' h 'hours'");
+ 
   const position = [lat, lng];
   
-  
-  
-
   return (
 <>
-<Flex background="black">
-<Card background="black" >
+<Flex >
+<Card background="whitesmoke" >
 <CardBody>
-<Text color="whitesmoke">Location: {city}, {country}</Text>
-<Text color="whitesmoke">IP: {ip}</Text>
+
+<List spacing={3} >
+
+<Text color="black">IP: {ip}</Text>
+
+<Text color="black">Location: {city}, {country}</Text>
+
+<Image src={flag}></Image>
+<Text color="black">Time: {localNowString}</Text>
+<Text color="black">Buenos Aires time difference: {timeDiffString}</Text>
+
+</List>
+
 </CardBody>
 </Card>
+<Box>
+</Box>
 </Flex>
 
   
@@ -48,12 +74,20 @@ const Ip = () => {
   />
   <Marker position={position}>
     <Popup>
-    {[lat, lng]}<br /> {city}, {region}, {country}
+    {[lat, lng]}  <br /> {city}, {region}, {country}
     </Popup>
   </Marker>
 </MapContainer>
+
+
+
 </>
   );
 };
 
+
 export default Ip;
+
+
+
+
